616326f26ad85d71011d21b3f15f30c181b1fe58
PRAJNA R PATIL <93109368+prajna0906@users.noreply.github.com>
2022-08-06 22:14:32 +0530

    Update App.tsx

--- fluent-ui/src/App.tsx
+++ fluent-ui/src/App.tsx
@@ -1,20 +1,16 @@
-import { makeStyles } from "@fluentui/react-components";
 import { TextField } from '@fluentui/react/lib/TextField';
 
-const useStyles = makeStyles({
-  root: {
-    display: "flex",
-    justifyContent: "center",
-    maxwidth: "400px",
-    marginTop: "25px"
-  }
-});
+const style = {
+  display: "flex",
+  justifyContent: "center",
+  maxwidth: "400px",
+  marginTop: "25px"
+};
 
-export const Default = () => {
-  const styles = useStyles();
+export const App = () => {
   return (
-    <div className={styles.root}>
-      <TextField type="file" /> 
+    <div style = { style }>
+      <TextField type="file" />
     </div>
   );
 };
