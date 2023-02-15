4c2bc77dafb56d4f0b32b0b30609ee57126bccbf
PRAJNA R PATIL <93109368+prajna0906@users.noreply.github.com>
2023-02-15 16:01:19 +0530

    Update console.js

--- console.js
+++ console.js
@@ -1,2 +1,7 @@
 //hey this is comment
 console.log("hello world")
+sion": "7.18.6",
+      "resolved": "https://registry.npmjs.org/@babel/code-frame/-/code-frame-7.18.6.tgz",
+      "integrity": "sha512-TDCmlK5eOvH+eH7cdAFlNXeVJqWIQ7gW9tY1GJIpUtFb6CmjVyq2VM3u71bOyR8CRihcCgMUYoDNyLXao3+70Q==",
+      "dependencies": {
+        "@babel/highlight": "^7.18.6"
